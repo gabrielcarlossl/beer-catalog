fcd9ee0ee3769d4bb3986c22835bf32275a92279
Gabriel Carlos <gabrielcarlossleite@gmail.com>
2023-06-30 20:56:19 -0300

    added global css

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../../assets/images/BearBeer.png'
+import '../../assets/styles/navbar.styles.css'
 
 const NavBar = () => {
   return (
 
     <nav>
-      <div>
+      <div className='nav-container'>
         <div>
           <Link to='/'>
             <img className='logo' src={logo} width={50} alt="student" />
           </Link>
         </div>
-        <div>
+        <div className='linkContainer'>
           <Link to='/'>Home Page</Link>
           <Link to='/beers-catalog'>Beer Catalog</Link>
         </div>
