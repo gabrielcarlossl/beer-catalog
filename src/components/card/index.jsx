ee08289026968ebe8cf3e348c075a26c1505b8cf
Gabriel Carlos <gabrielcarlossleite@gmail.com>
2023-07-01 00:26:42 -0300

    added card to show all beers

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchBeersRequest } from '../../redux/actions/beerActions'
 
-const card = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const Card = () => {
+    const dispatch = useDispatch();
+    const beers = useSelector((state) => state.beers.beers)
+    useEffect(() => {
+        dispatch(fetchBeersRequest())
+    }, [dispatch])
+    console.log(beers)
+
+    return (
+        <div>
+            {beers.map((beer) => (
+                <div key={beer.id}>
+                    <h3>{beer.name}</h3>
+                    <img src={beer.image_url} alt={beer.name} />
+                    <p>{beer.description}</p>
+                </div>
+            ))}
+        </div>
+    )
 }
 
-export default card
+export default Card
--- src/pages/catalog.jsx
+++ src/pages/catalog.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../assets/styles/catalog.styles.css'
+import Card from '../components/card'
+
 
 const Catalog = () => {
+  
+
   return (
     <div className='catalogContainer'>
       <h1>Catálogo de Cervejas</h1>
+      <div>
+        <Card></Card>
+      </div>
     </div>
   )
 }
