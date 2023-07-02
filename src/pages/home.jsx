894fee70e991c8219370611894af2529d487bb78
Gabriel Carlos <gabrielcarlossleite@gmail.com>
2023-07-02 11:14:48 -0300

    added Link to beer catalog page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { icon, style } from '../assets/styles/home.style'
 import ArrowOutwardIcon from '@mui/icons-material/ArrowOutward'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
     <div className={style}>
       <h1>Bem-Vindo a Loja <br /> Bear Beer</h1>
-      <a href="/beers-catalog">Acesse nosso catalogo de Cervejas <span className={icon}><ArrowOutwardIcon fontSize='medium'></ArrowOutwardIcon></span></a>
+      <Link to="/beers-catalog">Acesse nosso catalogo de Cervejas <span className={icon}><ArrowOutwardIcon fontSize='medium'></ArrowOutwardIcon></span></Link>
     </div>
   )
 }
