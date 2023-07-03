1ceb4e971a45189250bbf9ff5bf589c4eff2ee4c
Gabriel Carlos <gabrielcarlossleite@gmail.com>
2023-07-02 21:33:56 -0300

    edited catalog page colors

--- src/assets/styles/catalog.style.js
+++ src/assets/styles/catalog.style.js
@@ -4,21 +4,21 @@ export const catalogContainer = css`
   display: flex;
   flex-direction: column;
   align-items: center;
-  .cardSection {
-    background-color: rgb(206,204,247, 0.2);
-    border-radius: 20px;
-  }
+  background-color: #76CFA5;
+  height: 100vh;
   h1 {
     font-weight: 800;
     font-size: 64px;
     line-height: 38px;
     letter-spacing: 3.2px;
-    color: #fff71c;
-    -webkit-text-stroke: 2px #6602fa;
+    color: #F0951F;
+    -webkit-text-stroke: 2px #5C4527;
     @media (max-width: 500px) {
       font-size: 40px;
       text-align: center;
       line-height: 120%;
+      margin-bottom: 10px;
+      margin-top: 20px;
     }
   }
 `
