83da021a384203599668d1b0495e671543b4cc09
Gabriel Carlos <gabrielcarlossleite@gmail.com>
2023-07-02 21:34:14 -0300

    edited home page colors and responsive layout

--- src/assets/styles/home.style.js
+++ src/assets/styles/home.style.js
@@ -3,21 +3,40 @@ import { css } from '@emotion/css'
 export const style = css`
   display: flex;
   flex-direction: column;
-  margin-top: 50px;
   text-align: center;
-  height: 90vh;
+  height: 100vh;
+  overflow-y: hidden;
+  background-color: #76CFA5;
+  align-items: center;
+  .bear{
+    width: 500px;
+    margin-top: 50px;
+    border-radius: 40px;
+    border: 2px solid #5C4525;
+    @media (max-width: 500px) {
+      width: 350px;
+      margin-top: 20px;
+    }
+  }
   a {
-    color: #5c3c27;
+    color: #5C4527;
     font-size: 24px;
     font-weight: 600;
     margin-top: 50px;
     text-decoration: none;
     @media (max-width: 500px) {
       font-size: 20px;
+      margin-top: 20px;
     }
   }
   h1 {
-    color: #8119FF;
+    margin-top: 100px;
+    color: #5C4525;
+    font-weight: 700;
+    @media (max-width: 500px) {
+      
+      margin-top: 30px;
+    }
   }
   a:hover {
     text-decoration: underline;
