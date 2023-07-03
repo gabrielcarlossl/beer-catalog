ea32934812c158c862cfe673dceeefa471a1b3d6
Gabriel Carlos <gabrielcarlossleite@gmail.com>
2023-07-02 22:44:10 -0300

    added comments for code explanation

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -9,28 +9,33 @@ import Modal from 'react-modal'
 import { modalStyle } from '../../assets/styles/modal.style.js'
 
 const Card = () => {
-    const dispatch = useDispatch();
-    const [currentPage, setCurrentPage] = useState(1)
-    const [beersPerPage, setBeersPerPage] = useState(4)
-    const beers = useSelector((state) => state.beers.beers)
-    const [modalIsOpen, setModalIsOpen] = useState(false)
-    const [selectedBeer, setSelectedBeer] = useState(null)
+    const beers = useSelector((state) => state.beers.beers) // utilização do estado da store para acessar as cervejas que vieram da requisição
+    const dispatch = useDispatch()
 
+    const [currentPage, setCurrentPage] = useState(1) // estado para controlar pagina atual
+    const [beersPerPage, setBeersPerPage] = useState(4) // estado para controlar o número de cervejas a serem exibidos por página
+    const [modalIsOpen, setModalIsOpen] = useState(false) // controle do modal
+    const [selectedBeer, setSelectedBeer] = useState(null) // controle da seleção de cerveja
+
+    // realizar a requsição sempre que a pagina for montada, o useEffect é executado sempre que o dispatch mudar, já que ele está dentro do array.
     useEffect(() => {
         dispatch(fetchBeers());
     }, [dispatch])
 
+
+    // fuseEffect para alterar a quantidade de cervejas a serem exibidas por página dependendo do tamanho da tela, ele é executado assim que o componente é montado
     useEffect(() => {
+
         const handleResize = () => {
-            const screenWidth = window.innerWidth;
+            const screenWidth = window.innerWidth
             if (screenWidth <= 500) {
                 setBeersPerPage(1)
             } else {
                 setBeersPerPage(4)
             }
-        };
+        }
 
-        handleResize();
+        handleResize()
         window.addEventListener('resize', handleResize)
 
         return () => {
@@ -64,49 +69,65 @@ const Card = () => {
 
     // Renderizar os botões de paginação
     const renderPaginationButtons = () => {
-        const totalBeers = beers.length;
-        const totalPages = Math.ceil(totalBeers / beersPerPage)
+        const totalBeers = beers.length // recebe o numero total de cervejas para serem mostradas
+        const totalPages = Math.ceil(totalBeers / beersPerPage) // calcula quantas paginas serão de acordo com o número de cervejas por página que foi escolhido no setBeersPerPage. ceil arredonda para cima divisões com resto
         const currentPageIndex = currentPage - 1
 
         if (totalPages <= 1) {
-            return null
+            return null // se não tiver mais páginas retorna null para não mostrar números de paginação
         }
 
+        // Lista de botões de paginação
         const paginationButtons = []
-        let leftButtonNumber = currentPageIndex - 1
-        let rightButtonNumber = currentPageIndex + 1
 
-        if (currentPageIndex === 0) {
-            leftButtonNumber = currentPageIndex + 1
-            rightButtonNumber = currentPageIndex + 2
-        } else if (currentPageIndex === totalPages - 1) {
-            leftButtonNumber = currentPageIndex - 2
-            rightButtonNumber = currentPageIndex + 1
+        const maxButtons = 5 // Número máximo de botões de paginação para mostrar
+
+        // botão inicia deve ser igual ao index da pagina atual subtraido da metade do numero máximo de botões
+        let startButtonNumber = currentPageIndex - Math.floor(maxButtons / 2)
+        // botão final deve ser igual a soma do index da pagina atual mais a metade do numero máximo de botões
+        let endButtonNumber = currentPageIndex + Math.floor(maxButtons / 2)
+
+        console.log('botao final',endButtonNumber)
+        console.log('index da pagina atual',currentPageIndex)
+        console.log('botao inicial',startButtonNumber)
+
+
+        if (startButtonNumber < 0) {
+            // se o botão inicial for menor que 0 ele automaticamente é o botão incial então será mudado para 0
+            startButtonNumber = 0
+
+            // botão final será o menor valor entre o valor máximo de botões menos 1 e total de paginas menos 1, dessa forma sempre mostrará os botões de paginação quando estiver na página 1
+            endButtonNumber = Math.min(maxButtons - 1, totalPages - 1)
+
+            // se o botão final for maior ou igual ao total de paginas, o botão final será alterado para o numero total de paginas menos 1, e o botão inicial será alterado para o maior valor entre 0 e o numero do botão final menos o numero máximo de botões menos 1, dessa forma sempre mostrará os botões de paginação quando estiver na ultima página.
+        } else if (endButtonNumber >= totalPages) {
+            endButtonNumber = totalPages - 1
+            startButtonNumber = Math.max(0, endButtonNumber - (maxButtons - 1))
         }
 
-        // Função para gerar os botões de páginação
-        for (let i = leftButtonNumber; i <= rightButtonNumber; i++) {
-            if (i >= 0 && i < totalPages) {
-                const pageNumber = i + 1;
-                const isCurrentPage = pageNumber === currentPage;
+        // Função para gerar os botões de paginação
+        for (let i = startButtonNumber; i <= endButtonNumber; i++) {
+            const pageNumber = i + 1
+            
+            // verificação se a pagina é atual
+            const isCurrentPage = pageNumber === currentPage
 
-                paginationButtons.push(
-                    <button
-                        key={pageNumber}
-                        onClick={() => setCurrentPage(pageNumber)}
-                        disabled={isCurrentPage}
-                    >
-                        {pageNumber}
-                    </button>
-                )
-            }
+            paginationButtons.push(
+                <button
+                    key={pageNumber}
+                    onClick={() => setCurrentPage(pageNumber)} // quando botão é clicado ele passa a ser a página atual e desabilitado quando for a página atual
+                    disabled={isCurrentPage}
+                >
+                    {pageNumber}
+                </button>
+            )
         }
 
-        return paginationButtons
+        return paginationButtons;
     }
 
     return (
-        <div style={{ display: 'flex', flexDirection: 'column' }}>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
 
             <div className={cardStyle}>
                 {currentBeers.map((beer) => (
@@ -131,9 +152,9 @@ const Card = () => {
                                 <h2>Name: <span>{selectedBeer.name}</span></h2>
                                 <p>Tagline: <span>{selectedBeer.tagline}</span></p>
                             </div>
-                            <hr style={{ marginTop: 20, marginBottom: 20}} />
+                            <hr style={{ marginTop: 20, marginBottom: 20 }} />
                             <p>Description:</p> <span>{selectedBeer.description}</span>
-                            <hr style={{ marginTop: 20, marginBottom: 20}} />
+                            <hr style={{ marginTop: 20, marginBottom: 20 }} />
                             <img src={selectedBeer.image_url} alt={selectedBeer.name} />
                             <hr />
                         </div>
